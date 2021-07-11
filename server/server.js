3f1e7fcd625d832bf8b49d1e8da8dfedf4cf0e65
Bay Bui <baybui@ucla.edu>
2021-07-11 09:27:38 -0700

    updated package

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const path = require("path");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
-const path = require("path");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
